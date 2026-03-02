8c21e85d107fb781e33002768a78567f0ebcaac6
Dani F. <114934334+Yuf0ria@users.noreply.github.com>
2026-03-02 16:31:07 +0800

    Scripts for Week 8

---
+++ Assets/UISCript_DogBreed.cs
@@ -0,0 +1,34 @@
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UISCript_DogBreed : MonoBehaviour
+{
+    //Content
+    public TextMeshProUGUI pageCountText;
+    public Button previousButton,  nextButton;
+    //API_Content
+    public Transform textContainer; //Canvas Position
+    public GameObject textPrefab; //para sa text
+    public const float spaceInText = 2f; //space pag nag show ang text
+    //Camera
+    public Camera cam;
+    public const float camMoveSpeed = 10f;
+    //pages
+    private List<string> allTexts = new List<string>();
+    private int contentPerPage = 10, currentPage = 10, totalPages = 0;
+    
+    string URL = "https://dog.ceo/api/breeds/list/all";
+
+    void Start()
+    {
+        //on click previoosu btn
+        //on clicks nex tbtn
+        
+        //once click fetch aopi
+    }
+    
+    
+
+}
